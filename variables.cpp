6dc9fbd3e4acfad8fe14b8b39ff0f76a8b615fb2
Rosa Palaci <palacibelrose431@gmail.com>
2022-09-18 17:49:41 -0500

    Clase variables

---
+++ variables.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+using namespace std;
+
+int main(){
+    cout << "Holis, my name is Rosa Palacios\n";
+    /*Creando nuestras primeras variables: 
+    1. Indicando el tipo de dato que vamos almacenar
+    2. Un nombre para la variable
+    Extra: le podemos dar un valor inicial (es buena practica)*/
+    int age = 21; // sin asignación de valor inicial la variable imprime la dirección en memoria esto es una lógica propio falta verificar esta información
+    age = 22; // reasignación de valores
+    cout << "I have "<< age << " years";
+    /*Las variables pueden ser manipuladas o mostradas al usuario
+    Para modificarlas con el signo =*/
+}
