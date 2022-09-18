66a66ccaa870727a7f06337a562034c5da77c63b
Rosa Palaci <palacibelrose431@gmail.com>
2022-09-18 17:22:13 -0500

    Tipos de datos

---
+++ main.cpp
@@ -0,0 +1,24 @@
+#include <iostream>
+//using namespace std;
+using std::cout;
+using std::endl;
+
+int main(){
+    cout<<"Hello World :D\n";
+    int boolean = false;
+    cout<< boolean << endl;
+
+    int number1 = 170/8; //respuesta 21.25 pero solo toma la parte entera que es 21
+    cout << number1 << endl;
+
+    float number2 = 5.23;
+    cout << number2 <<endl;
+
+    double number3 = 44.5691093;
+    cout << number3 <<endl;
+
+    char letra = 'R'; //Funciona con letras minusculas y mayusculas
+    cout << letra <<endl;
+
+    cout<<"Bye World :D \nBy Rosa Palacios";
+}
