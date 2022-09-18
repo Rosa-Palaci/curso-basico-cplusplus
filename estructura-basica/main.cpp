f8d74d5757ec63cabb351771a1c33c8ca5aa3bdc
Rosa Palaci <palacibelrose431@gmail.com>
2022-09-18 16:55:17 -0500

    Anotaciones teoricas y expllicativas de la estructura basica de un programa en C++

--- estructura-basica/main.cpp
+++ estructura-basica/main.cpp
@@ -1,6 +1,7 @@
 #include <iostream>
 using namespace std;
 
-int main(){
-    cout<< "Hola mundo, Soy Rosa Palacios estudiante en Platzi";
+int main(){ //La función main es conocido como punto de entrada ya que es la primera que se ejecutará 
+    cout<< "Hola mundo, Soy Rosa Palacios estudiante en Platzi"; //Cadena de texto siempre entre comillas dobles
 }
+//Para correr el programa con F6 sobre la terminal
