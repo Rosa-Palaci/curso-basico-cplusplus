580fd982460a26fa74853b4745ef112854309273
Rosa Palaci <palacibelrose431@gmail.com>
2022-09-29 14:41:35 -0500

    Se termino leccion condicional switch case

--- entrada-salida.cpp
+++ entrada-salida.cpp
@@ -3,8 +3,18 @@ using namespace std;
 
 int main(){
     int option = 0;
+    cout << "Enter an option: "<<endl;
+    cin >> option;
     //Validar la solucion de usuario
-    if(option == 1 ){
-        cout<<"First option";
-    } // sabiendo que es este el patro con switch-case lo mejoramos
+    switch (option)
+    {
+    case 1:
+        cout << "Option 1"<<endl;
+        break; //una vez que termine de ejecutar termine el switch o condicional
+    case 2:
+        cout << "Option 2"<<endl;
+        break;
+    default:
+        cout << "Invalid option"<<endl;
+    }
 }
