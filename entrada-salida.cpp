c37f54e9719f0ca10457e9e1995ce2e4bacb5bde
Rosa Palaci <palacibelrose431@gmail.com>
2022-09-29 14:31:44 -0500

    Se agrego archivo entrada-salida para ver el condicional switch case

---
+++ entrada-salida.cpp
@@ -0,0 +1,10 @@
+#include <iostream>
+using namespace std;
+
+int main(){
+    int option = 0;
+    //Validar la solucion de usuario
+    if(option == 1 ){
+        cout<<"First option";
+    } // sabiendo que es este el patro con switch-case lo mejoramos
+}
