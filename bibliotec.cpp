b813fc339e6ba4050c70388fd03c1653c27e71ff
Rosa Palaci <palacibelrose431@gmail.com>
2022-10-01 15:43:13 -0500

    se incluyo la libreria de string para genera una cadena de texto de una forma mas eficiente

--- bibliotec.cpp
+++ bibliotec.cpp
@@ -1,7 +1,8 @@
 #include <iostream>
+#include <string> // nos permite declarar un nuevo tipo de dato para las variables
 using namespace std;
 
 int main(){
-    char texto[] = {'H', 'o', 'l', 'a'};//esto funciona de escribir texto aunque poco eficiente
+    string texto = "Hola chicos";//esto funciona de escribir texto aunque poco eficiente
     cout << texto;
 }
