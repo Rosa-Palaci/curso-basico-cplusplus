ebb2e445c818952112df7c3acc610b76f152adac
Rosa Palaci <palacibelrose431@gmail.com>
2022-10-01 15:56:22 -0500

    funcion stof cambia el texto a float

--- biblioteca.cpp
+++ biblioteca.cpp
@@ -3,6 +3,6 @@
 using namespace std;
 
 int main(){
-    string texto = "10";//esto funciona de escribir texto aunque poco eficiente
-    cout << stoi(texto)*2;
+    string texto = "10.4";//esto funciona de escribir texto aunque poco eficiente
+    cout << stof(texto)*2.3;
 }
