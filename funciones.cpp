986649a1dbaadfd701d5ce7bd79cd919f4becc21
Rosa Palaci <palacibelrose431@gmail.com>
2022-10-01 15:28:25 -0500

    valor por defecto desde la funcion

--- funciones.cpp
+++ funciones.cpp
@@ -5,10 +5,11 @@ int suma(int a, int b){
     return a + b ;
 }//funcion de suma de dos valores
 
-float multiplicacion (float a, float b){
+float multiplicacion (float a, float b = 2){
     return a * b;
 }
+
 int main(){
-    cout<< suma(2, 782)<<endl;
-    cout << multiplicacion(45.3, 2.2)<<endl;
+    //cout<< suma(2, 782)<<endl;
+    cout << multiplicacion(200,6)<<endl;
 }
