af6075ccc56af953d4cfd8d2ff26f8f843e6eaec
Rosa Palaci <palacibelrose431@gmail.com>
2022-10-01 15:55:21 -0500

    funcion stoi cambia el texto a numero

--- biblioteca.cpp
+++ biblioteca.cpp
@@ -3,6 +3,6 @@
 using namespace std;
 
 int main(){
-    string texto = "Hola chicos";//esto funciona de escribir texto aunque poco eficiente
-    cout << texto.size();
+    string texto = "10";//esto funciona de escribir texto aunque poco eficiente
+    cout << stoi(texto)*2;
 }
