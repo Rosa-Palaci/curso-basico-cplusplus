27f8da7a136f8357e178a905c6377d2b2a6d3ac5
Rosa Palaci <palacibelrose431@gmail.com>
2022-10-01 15:25:56 -0500

    funcion de multiplicacion de dos numeros decimales

--- funciones.cpp
+++ funciones.cpp
@@ -4,6 +4,11 @@ using namespace std;
 int suma(int a, int b){
     return a + b ;
 }//funcion de suma de dos valores
+
+float multiplicacion (float a, float b){
+    return a * b;
+}
 int main(){
     cout<< suma(2, 782)<<endl;
+    cout << multiplicacion(45.3, 2.2)<<endl;
 }
