6f2cdcd39388dc8fed443731ed64d80ee11c0449
Rosa Palaci <palacibelrose431@gmail.com>
2022-09-30 22:01:10 -0500

    Tabla de multiplicar con una lista y el ciclo for

--- ciclo-for.cpp
+++ ciclo-for.cpp
@@ -2,10 +2,13 @@
 using namespace std;
 
 int main (){
-    int limite = 7;
+    int list_num[] = {1,2,3,4,5,6,7,8,9,10};
+    int limite = 0;
     cout << "Limite: "<<endl;
     cin >> limite;
-    for (int i = 0; i < limite; i += 1){
-        cout<<i<<endl;
+    cout<<"\n";
+    
+    for (int i = 1; i < limite; i += 1){
+        cout<<list_num[i]*2<<endl;
     }
 }
