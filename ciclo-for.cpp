8985c95faa952c324ad1566be3dd2de2b40d518a
Rosa Palaci <palacibelrose431@gmail.com>
2022-10-01 14:32:29 -0500

    Se cumplio el reto de imprimir numeros pares de una tabla de multiplicar, aqui solo imprime los numeros pares de la tabla de multiplicar 7 que son: 14, 28, 42, 56, 70

--- ciclo-for.cpp
+++ ciclo-for.cpp
@@ -5,10 +5,10 @@ int main (){
     int list_num[] = {1,2,3,4,5,6,7,8,9,10};
     int limite = sizeof(list_num) / sizeof(list_num[0]); //sizeof tamaño de la variable, 4 bits por entero
     for (int i = 0; i < limite; i += 1){
+        list_num[i]= list_num[i]*7;
+        //cout << list_num[i]<<endl;
+        if(list_num[i] % 2 == 0){
         cout<<list_num[i]<<endl;
-        if(list_num[i] == 5){
-        break;
+        }
     }
-    }
-
 }
--- tempCodeRunnerFile.cpp
+++ tempCodeRunnerFile.cpp
@@ -1,3 +1,14 @@
-else {
-            cout <<"No puedes votar :'C ";
+#include <iostream>
+using namespace std;
+
+int main (){
+    int list_num[] = {1,2,3,4,5,6,7,8,9,10};
+    int limite = sizeof(list_num) / sizeof(list_num[0]); //sizeof tamaño de la variable, 4 bits por entero
+    for (int i = 0; i < limite; i += 1){
+        list_num[i]= list_num[i]*8;
+        //cout << list_num[i]<<endl;
+        if(list_num[i] % 2 == 0){
+        cout<<list_num[i]<<endl;
         }
+    }
+}
